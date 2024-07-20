64133860537231e54c43f6f111571d6980696f33
Xin Chen <chenxin19920507@gmail.com>
2024-07-20 19:45:35 -0400

    Update Java8Test.java

--- CoreJavaDemo-main/src/main/java/org/example/day3/Java8Test.java
+++ CoreJavaDemo-main/src/main/java/org/example/day3/Java8Test.java
@@ -4,6 +4,18 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Java8Test {
+    /*
+     *  java.util.function
+     *
+     *  Supplier <T>    T get();    // return T (generic type)
+     *
+     *  Consumer <T>    void accept(T t);
+     *
+     *  Predicate <T>   boolean test(T t);
+     *
+     *  Function <T, R>     R apply(T t);
+     *  // T is for input, R is for output
+     */
 /*
 *   1.  
 *   2.
